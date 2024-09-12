b0bf9fcaad2af9d5d8fc4cd8be7083c1df0f1afd
Vinit Kumar <98470219+vinitisready@users.noreply.github.com>
2024-09-12 23:04:23 +0530

    Update script.js
    Fixed the typing section loading issues

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Student", "Developer", "Engineer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
